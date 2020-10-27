0baf0801af4c14cdcfed05ee3318ded59cb0c206
Michael Zaslavsky <mikezasl@gmail.com>
2020-10-27 01:23:17 -0500

    changed to a post

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -11,7 +11,7 @@ export default {
   },
   // Deletes the saved book with the given id
   deleteBook: function(id) {
-    return axios.delete("/api/books/" + id);
+    return axios.post("/api/books/" + id);
   },
   // Saves an book to the database
   saveBook: function(bookData) {
